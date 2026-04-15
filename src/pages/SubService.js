68642dccba3b36753cc239c6827f60d5fd099936
Shree Laxmi Admin <admin@shreelaxmigarage.com>
2026-04-15 18:24:27 +0530

    fix: remove redundant role

--- src/pages/SubService.js
+++ src/pages/SubService.js
@@ -29,12 +29,15 @@ const logos = (rawClientLogos || []).map((item) => ({
 const SubService = () => {
   const { id, subId } = useParams();
 
-  const data = useMemo(() => services.find((s) => s.id === id), [subId]);
+ const data = useMemo(
+  () => services.find((s) => s.id === id),
+  [id]
+);
 
-  const subData = useMemo(
-    () => data.subServices.find((d) => d.id === subId),
-    [subId],
-  );
+const subData = useMemo(
+  () => data?.subServices?.find((d) => d.id === subId),
+  [subId, data]
+);
 
   return (
     <>
